c8d3189f54f8a36d3c95b072f89cc49dd2924120
Pete Smith <petejosephsmith@gmail.com>
2019-07-26 11:33:40 -0400

    add array

--- js/peteSmithCodeLou.js
+++ js/peteSmithCodeLou.js
@@ -1,11 +1,39 @@
-$('./!CSSCLASS!/').hide;
-$('.cssParent child');
+// $socialBanner = newArray banner=0
 
-$("#target").click(function() {
-    alert( "Handler for .click() called."");
-});
-append("<hr />");
+// $(document).ready(function(){
+//   $("logo-button").click(function(){
+//     $("img").attr("width",function(n, v){
+//       return v - 50;
+        
+// function showSocial() {
+//    for(var i = 0; i < 5; i++) {
+//       document.log(i); 
+//    }
+//    console.log(i); //prints 5;
+// }
+// function socialIcons(){
+// for (i = 0, i < newArray.length+1, i =+1)
+//         { document.display(socialBanner[i]);
+//        }
+         
+$MyBanner=newArray('facebook.png','twiiter.png','linkedin.png','instagram.png')
+banner=0
+function ShowBanners()
+{ if (document.images)
+{ banner++
+if (banner==MySocial.length) {
+banner=0}
+document.ChangeBanner.src=MyBanners[banner]
+setTimeout("ShowBanners()",1000)
+}
+// $('./!CSSCLASS!/').hide;
+// $('.cssParent child');
+
+// $("#target").click(function() {
+//     alert( "Handler for .click() called."");
+// });
+// append("<hr />");
 
 
-jQuery("ul li").append("hr/>")
+// jQuery("ul li").append("hr/>")
 
