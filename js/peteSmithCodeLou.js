64657655bcac44117c22504967689d6ebf856e75
petejsmith <petejosephsmith@gmail.com>
2019-07-26 07:40:54 -0400

    CodeLouPreFinalCommit

---
+++ js/peteSmithCodeLou.js
@@ -0,0 +1,10 @@
+$('./!CSSCLASS!/').hide;
+$('.cssParent child');
+
+$("#target").click(function() {
+    alert( "Handler for .click() called."");
+});
+append("<hr />");
+
+
+jQuery("ul li").append("hr/>")
